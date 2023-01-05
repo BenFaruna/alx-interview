72730afbf5653222fee6fef1f274f6d398df934f
BenFaruna <benjaminfaruna@gmail.com>
2023-01-06 00:56:37 +0100

    Added documentation to canUnlockAll function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""LockBoxes Algorithm"""
 def canUnlockAll(boxes):
+    """determine if all boxes can be opened"""
     unlocked = [0]
     for box_id, box in enumerate(boxes):
         if not box:
