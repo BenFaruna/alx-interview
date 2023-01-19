6e00ff236e75212ae071cbee993da64478b8afb0
BenFaruna <benjaminfaruna@gmail.com>
2023-01-20 00:25:12 +0100

    Arranged output in ascending order

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,9 +6,10 @@ import sys
 def print_stats(size: int, status_code: dict) -> None:
     """prints file and count of all status codes"""
     print("File size: {}".format(file_size))
-    for code, count in status_code.items():
-        if count > 0:
-            print("{}: {}".format(code, count))
+    codes = ["200", "301", "400",  "401", "403", "404", "405", "500"]
+    for code in codes:
+        if status_code[code] != 0:
+            print("{}: {}".format(code, status_code[code]))
 
 
 count = 0
@@ -27,7 +28,7 @@ status_code = {
 try:
     for line in sys.stdin:
         line_n = line.split()
-        if 9 == len(line_n):
+        if len(line_n) == 9:
             count += 1
             file_size = file_size + int(line_n[-1])
             code = line_n[-2]
