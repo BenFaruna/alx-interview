a9edbd72a29a5a34def72e647c0b96d9000dd88c
BenFaruna <benjaminfaruna@gmail.com>
2023-02-07 14:56:05 +0100

    Made code style compliant

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,30 @@
 #!/usr/bin/node
 const request = require('request');
 
-const api_url = 'https://swapi-api.alx-tools.com/api';
+const apiUrl = 'https://swapi-api.alx-tools.com/api';
 
 if (process.argv.length > 2) {
-    const film_url =  api_url + '/films/' + process.argv[2];
-    request.get(film_url, (error, res, body) => {
+  const filmUrl = apiUrl + '/films/' + process.argv[2];
+  request.get(filmUrl, (error, res, body) => {
+    if (error) {
+      console.log('Error:', error.message);
+      return;
+    }
+
+    body = JSON.parse(body);
+    const characters = body.characters;
+    characters.forEach(characterUrl => {
+      request.get(characterUrl, (error, res, body) => {
         if (error) {
-            console.log("Error:", error.message);
-            return;
+          console.log('Error:', error);
+          return;
         }
 
         body = JSON.parse(body);
-        const characters = body['characters'];
-        characters.forEach(character_url => {
-            request.get(character_url, (error, res, body) => {
-                if (error) {
-                    console.log('Error:', error);
-                    return;
-                }
-
-                body = JSON.parse(body);
-                console.log(body['name']);
-            });
-            
-        });
-    })
+        console.log(body.name);
+      });
+    });
+  });
 } else {
-    console.log('Usage: film id required');
-    return;
+  console.log('Usage: film id required');
 }
