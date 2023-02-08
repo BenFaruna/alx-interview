b46ef4bfa9095d9f51bddfdb5832ce43311137d6
BenFaruna <benjaminfaruna@gmail.com>
2023-02-08 23:35:43 +0100

    Completed 0x06-starwars_api using promise

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,26 +5,36 @@ const apiUrl = 'https://swapi-api.alx-tools.com/api';
 
 if (process.argv.length > 2) {
   const filmUrl = apiUrl + '/films/' + process.argv[2];
-  request.get(filmUrl, (error, res, body) => {
+  request.get(filmUrl, async (error, res, body) => {
     if (error) {
       console.log('Error:', error.message);
       return;
     }
 
+    const characterArr = [];
     body = JSON.parse(body);
-    const characters = body.characters;
-    characters.forEach(characterUrl => {
-      request.get(characterUrl, (error, res, body) => {
-        if (error) {
-          console.log('Error:', error);
-          return;
-        }
+    let characters = body.characters;
+    for (const characterLink of characters) {
+      characterArr.push(character(characterLink));
+    }
 
-        body = JSON.parse(body);
-        console.log(body.name);
-      });
+    characters = await Promise.all(characterArr);
+
+    characters.forEach(character => {
+      console.log(JSON.parse(character).name);
     });
   });
 } else {
   console.log('Usage: film id required');
+}
+
+function character (characterLink) {
+  return new Promise((resolve, reject) => {
+    request(characterLink, (error, res, data) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(data);
+    });
+  });
 }
